1d26e5ffe28fa164318675d30ba64d8717d0582a
Michman <raspberras@gmail.com>
2022-05-05 04:40:34 +0300

    added autotest, need to rearrange the code

--- main_azimuth.py
+++ main_azimuth.py
@@ -5,6 +5,67 @@
 import math
 import os
 import sys
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+emptyWord = 'empty'
+pointsDelta = [emptyWord, emptyWord]
+
+# define the scope
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+# add credentials to the account
+creds = ServiceAccountCredentials.from_json_keyfile_name('creds/azimuth1-0e58a3cdb4e1.json', scope)
+
+# authorize the clientsheet
+client = gspread.authorize(creds)
+
+sheet = client.open('testdata')
+
+# get the first sheet of the Spreadsheet
+sheet_instance = sheet.get_worksheet(0)
+
+
+# get the total number of columns
+print(sheet_instance.col_count)
+
+# get the value at the specific cell
+homePointCoordsAutotest = [sheet_instance.cell(4, 2).value, sheet_instance.cell(4, 3).value]
+print(homePointCoordsAutotest)
+# val = worksheet.acell('B1').value
+# https://docs.gspread.org/en/latest/user-guide.html
+
+for counter in range(8):
+    targetPointCoordsAutotest = [sheet_instance.cell(4 + counter, 4).value, sheet_instance.cell(4 + counter, 5).value]
+    for i in range(2):
+        pointsDelta[i] = float(targetPointCoordsAutotest[i].replace(',', '.'))-float(homePointCoordsAutotest[i].replace(',', '.'))
+
+    if pointsDelta[0] > 0:
+        if pointsDelta[1] > 0:
+            azimuth = math.degrees(math.atan(pointsDelta[0]/pointsDelta[1]))
+            print('A condition worked')
+        else:
+            azimuth = 90 + abs(math.degrees(math.atan(pointsDelta[1]/pointsDelta[0])))
+            print('C condition worked')
+    if pointsDelta[0] == 0:
+        if pointsDelta[1] > 0:
+            azimuth = 0
+            print('D condition worked')
+        if pointsDelta[1] == 0:
+            print('Home and Target coords are the same')
+            print('E condition worked')
+        if pointsDelta[1] < 0:
+            azimuth = 180.0
+            print('F condition worked')
+    if pointsDelta[0] < 0:
+        if pointsDelta[1] >= 0:
+            azimuth = 270 + abs(math.degrees(math.atan(pointsDelta[1]/pointsDelta[0])))
+            print('G condition worked')
+        else:
+            azimuth = 180 + abs(math.degrees(math.atan(pointsDelta[0]/pointsDelta[1])))
+            print('I condition worked')
+    sheet_instance.update_cell(4+counter, 11, azimuth)
 
 
 def check_input_correct(keyboard_input: str, empty_word):
